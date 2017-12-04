38882e4917189df4bf3e70117f53277b7b58817c
SawWu <2804265880@qq.com>
2017-12-04 21:56:21 +0800

    更新项目

---
+++ webpack-3/demo-36/src/index.js
@@ -0,0 +1,17 @@
+import css from './index.css'
+
+import img from './bd_logo1.png'
+
+import _ from 'lodash';
+
+function component() {
+    var element = document.createElement('div');
+
+    // Lodash, currently included via a script, is required for this line to work
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
--- webpack-3/demo-36/webpack.config.js
+++ webpack-3/demo-36/webpack.config.js
@@ -1,24 +1,59 @@
 var webpack = require("webpack");
 var path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
 
 module.exports = {
-  entry: {
-    index: './src/index.js'
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
+    devtool: 'eval-cheap-module-source-map',
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        filename: 'js/[name].bundle.js',
+        path: path.resolve(__dirname, 'dist/assets'),
+        publicPath: '/assets'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        //加载url-loader 同时安装 file-loader;
+                        loader : 'url-loader',
+                        options : {
+                            limit : 5000,
+                            //设置最终img路径;
+                            name : 'images/[name]-[hash].[ext]'
+                        }
+                    },
+                ]
+            }
+        ]
+    },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         open: true,
         port: 8888
     },
     plugins: [
+        new CleanWebpackPlugin('dist'),
         new HtmlWebpackPlugin({
-            filename: './index.html',
+            filename: '../index.html',
             template: 'src/index.html',
+            minify:{
+                removeAttributeQuotes: true
+            },
+            hash: true,
+            inject: 'body',
         })
     ]
 };
